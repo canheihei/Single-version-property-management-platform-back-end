2e441131f830896add09bf20d1500187e4b6053c
chhei <1835494827@qq.com>
2024-09-11 16:11:39 +0800

    项目初始化

---
+++ mall-ware/src/test/java/com/chhei/mall/ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chhei.mall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
