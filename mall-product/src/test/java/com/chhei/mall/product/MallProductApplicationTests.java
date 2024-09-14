ddd2369221102e2185bf974e76ef258aaeacb54f
chhei <1835494827@qq.com>
2024-09-14 13:43:27 +0800

    mybatis-plus整合

--- mall-product/src/test/java/com/chhei/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/chhei/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,30 @@
 package com.chhei.mall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.chhei.mall.product.entity.BrandEntity;
+import com.chhei.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MallProductApplicationTests {
-
+	@Autowired
+	BrandService brandService;
 	@Test
 	void contextLoads() {
+		BrandEntity entity = new BrandEntity();
+		entity.setName("魅族");
+		brandService.save(entity);
 	}
 
+	@Test
+	void selectById(){
+		List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id",2));
+		for (BrandEntity entity : list) {
+			System.out.println(entity);
+		}
+	}
 }
