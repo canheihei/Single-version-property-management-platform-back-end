a956a336fb8f3d1b222ddc1928213d4c234f63bb
chhei <1835494827@qq.com>
2024-11-22 22:04:50 +0800

    检索模块完成

--- mall-search/src/main/java/com/chhei/mall/mallsearch/controller/SearchController.java
+++ mall-search/src/main/java/com/chhei/mall/mallsearch/controller/SearchController.java
@@ -5,6 +5,7 @@ import com.chhei.mall.mallsearch.vo.SearchParam;
 import com.chhei.mall.mallsearch.vo.SearchResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,10 +13,11 @@ public class SearchController {
 	@Autowired
 	private MallSearchService mallSearchService;
 
-	@GetMapping("/list.html")
-	public String listPage(SearchParam param){
+	@GetMapping(value = {"/list.html","/","/index.html"})
+	public String listPage(SearchParam param, Model model){
+		// 通过对应的Service根据传递过来的相关的信息去ES中检索对应的数据
 		SearchResult search = mallSearchService.search(param);
-
+		model.addAttribute("result",search);
 		return "index";
 	}
 }
--- mall-search/src/main/java/com/chhei/mall/mallsearch/vo/SearchResult.java
+++ mall-search/src/main/java/com/chhei/mall/mallsearch/vo/SearchResult.java
@@ -2,17 +2,26 @@ package com.chhei.mall.mallsearch.vo;
 
 import com.chhei.common.dto.es.SkuESModel;
 import lombok.Data;
-
 import java.util.List;
 
 @Data
 public class SearchResult {
-	private List<SkuESModel> products;
-	private Integer pageNum;
-	private Long total;
+
+	private List<SkuESModel> products; // 查询到的所有的商品信息 满足条件
+	// 分页信息
+	private Integer pageNum; // 当前页
+	private Long total;  // 总的记录数
+	private Integer totalPages; // 总页数
+	private List<Integer> navs; // 需要显示的分页的页码
+
+	// 当前查询的所有的商品涉及到的所有的品牌信息
 	private List<BrandVO> brands;
+	// 当前查询的所有的商品涉及到的所有的属性信息
 	private List<AttrVo> attrs;
+
+	// 当前查询的所有商品涉及到的所有的类别信息
 	private List<CatalogVO> catalogs;
+
 
 	@Data
 	public static class CatalogVO{
@@ -20,12 +29,14 @@ public class SearchResult {
 		private String catalogName;
 	}
 
-
+	/**
+	 * 品牌的相关信息
+	 */
 	@Data
 	public static class BrandVO{
-		private Long brandId;
-		private String brandName;
-		private String brandImg;
+		private Long brandId; // 品牌的编号
+		private String brandName; // 品牌的名称
+		private String brandImg; // 品牌的图片
 	}
 
 	@Data
@@ -34,4 +45,5 @@ public class SearchResult {
 		private String attrName; // 属性的名称
 		private List<String> attrValue; // 属性的值
 	}
+
 }
