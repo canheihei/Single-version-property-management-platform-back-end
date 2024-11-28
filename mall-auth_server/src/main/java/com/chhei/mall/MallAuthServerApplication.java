dc2095c84b330b314f9c3d31436bfe31c494908e
chhei <1835494827@qq.com>
2024-11-29 01:15:18 +0800

    短信验证模块完成

---
+++ mall-auth_server/src/main/java/com/chhei/mall/MallAuthServerApplication.java
@@ -0,0 +1,15 @@
+package com.chhei.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MallAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallAuthServerApplication.class, args);
+	}
+
+}
